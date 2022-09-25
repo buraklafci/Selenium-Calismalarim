e1a6fb73fef5a24587b6d08c50c2c74b03b85181
burak <karanfilkaranfil822@gmail.com>
2022-09-25 23:57:37 +0300

    25.09.2022

--- src/test/java/calismalarim/C18_Faker.java
+++ src/test/java/calismalarim/C18_Faker.java
@@ -1,11 +1,18 @@
 package calismalarim;
 
+import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.security.Key;
 import java.time.Duration;
 
 public class C18_Faker {
@@ -22,22 +29,48 @@ public class C18_Faker {
 
     @Test
     public void isEnabled() {
+
         //Faker class’i testlerimizi yaparken ihtiyav duydugumuz isim, soyisim, adres vb bilgiler icin fake
         //degerler uretmemize imkan tanir.
         //Faker degerler uretmek icin Faker class’indan bir obje uretir ve var olan method’lari kullaniriz.
         //1. "https://facebook.com" Adresine gidin
         driver.get("https://facebook.com");
         //2. “create new account” butonuna basin
+        driver.findElement(By.xpath("(//a[@role='button'])[2]")).click();
         //3. “firstName” giris kutusuna bir isim yazin
+        Faker faker=new Faker();  //once kutuphaneye yukledik
+
+        String email = faker.internet().emailAddress();   //email iki defa ayni olmasi icin atama yapariz
+
+        Actions actions=new Actions(driver);
+        WebElement firstName=driver.findElement(By.xpath("(//input[@type='text'])[2]"));
+        //faker kutuphanesinin name methodundan isim ve soyismi rastgele atadik
+        actions.click(firstName).sendKeys(faker.name().name()).
         //4. “surname” giris kutusuna bir soyisim yazin
+        sendKeys(Keys.TAB).sendKeys(faker.name().lastName()).
         //5. “email” giris kutusuna bir email yazin
+        //internet methodundan emmail ve passwordu rastgele atadik
+        sendKeys(Keys.TAB).sendKeys(email).
         //6. “email” onay kutusuna emaili tekrar yazin
+        sendKeys(Keys.TAB).sendKeys(email).
         //7. Bir sifre girin
+        sendKeys(Keys.TAB).sendKeys(faker.internet().password()).
         //8. Tarih icin gun secin
+        sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys("02").
         //9. Tarih icin ay secin
+        sendKeys(Keys.TAB).sendKeys("Şub").
         //10. Tarih icin yil secin
+        sendKeys(Keys.TAB).sendKeys("1991").
         //11. Cinsiyeti secin
+        sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.RIGHT).
+        sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB).perform();
         //12. Isaretlediginiz cinsiyetin secili, diger cinsiyet kutusunun secili olmadigini test edin.
+        WebElement female=driver.findElement(By.xpath("//input[@value='1']"));
+        WebElement male=driver.findElement(By.xpath("//input[@value='2']"));
+        Assert.assertTrue(male.isSelected());
+        Assert.assertFalse(female.isSelected());
         //13. Sayfayi kapatin
+        driver.close();
     }
 }
--- src/test/java/calismalarim/C19_Summary.java
+++ src/test/java/calismalarim/C19_Summary.java
@@ -1,12 +1,36 @@
 package calismalarim;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class C19_Summary {
-    //1. "http://webdriveruniversity.com/Actions" sayfasina gidin
-    //2. "Hover over Me First" kutusunun ustune gelin
-    //3. "Link 1" e tiklayin
-    //4. Popup mesajini yazdirin
-    //5. Popup'i tamam diyerek kapatin
-    //6. "Click and hold" kutusuna basili tutun
-    //7. "Click and hold" kutusunda cikan yaziyi yazdirin
-    //8. "Double click me" butonunu cift tiklayin
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @Test
+    public void isEnabled() {
+        //1. "http://webdriveruniversity.com/Actions" sayfasina gidin
+        driver.get("http://webdriveruniversity.com/Actions");
+        //2. "Hover over Me First" kutusunun ustune gelin
+        //3. "Link 1" e tiklayin
+        //4. Popup mesajini yazdirin
+        //5. Popup'i tamam diyerek kapatin
+        //6. "Click and hold" kutusuna basili tutun
+        //7. "Click and hold" kutusunda cikan yaziyi yazdirin
+        //8. "Double click me" butonunu cift tiklayin
+    }
 }
